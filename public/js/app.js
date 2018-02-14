7e8f2b61dc4f67ec46e35f1f96c72bb7942d74ea
JessicaMP <jessica95.mp@gmail.com>
2018-02-14 02:11:31 -0500

    Funcionalidad con fetch

--- public/js/app.js
+++ public/js/app.js
@@ -39,59 +39,51 @@ function addNews() {
   responseContainer.appendChild(li);
 }
 // FETCH
-const url = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=4b7cef7670774d389866ae8119f77dac`;
 const formFetch = document.querySelector('#search-form-right');
 const searchFieldFetch = document.querySelector('#search-keyword-right');
 const responseContainerFetch = document.querySelector('#response-container-right');
-
 var btn = document.querySelector("#submit-btn-right");
+
+let searchedForTextFetch;
+
 btn.addEventListener("click", function(e){
   e.preventDefault();
   responseContainerFetch.innerHTML = '';
   searchedForTextFetch = searchField.value;
+  const url = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=4b7cef7670774d389866ae8119f77dac`;
+
   fetch(url)
-  .then(getNews2)
+  .then(handleErrors)
   .then(parseJSON)
   .then(addNews2)
   .catch(displayErrors);
 });
-
-function getNews2() {
-  const articleRequest = new XMLHttpRequest();
-  articleRequest.open(`GET`, url);
-  articleRequest.onload = addNews2;
-  articleRequest.onerror = handleErrors;
-
-}
 
 function handleErrors(res){
   if(!res.ok){
     throw Error(res.status);
   }
   return res;
-  console.log(res);
 }
 
 function parseJSON(res){
+  console.log(res);
   return res.json()
-    .then(function(parsedData){
+  .then(function(parsedData) {
     return parsedData.response.docs[0];
-    console.log(parsedData.response.docs[0]);
-    })
+  })
 }
 
 function addNews2(data){
-
+  //const article = data.response.docs[0];
   const  title = data.headline.main;
   const  snippet = data.snippet;
-  const article = data.docs[0];
 
   let li = document.createElement('li');
   li.className = 'articleClassFetch';
   li.innerText = snippet;
 
   responseContainerFetch.appendChild(li);
-
 }
 
 function displayErrors(err){
